dd130d52f234325e994c122dc5b11f6311255afd
sheben404 <sheben404@gmail.com>
2021-09-13 21:03:56 +0800

    feat: 利用 selector 实现精准渲染，组件只在自己的依赖的数据发生变化时重新渲染

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,16 @@ function App() {
 
 const Child1 = () => <section className={'child'}>child1<User/></section>;
 const Child2 = () => <section className={'child'}>child2<UserModifier/></section>;
-const Child3 = () => <section className={'child'}>child3</section>;
+const Child3 = connect((state: any) => {
+  return {group: state.group};
+})(({group}) => <section className={'child'}>child3
+  <div>Group:{group.name}</div>
+</section>);
 
 const User = connect((state: any) => {
-  return {user: state.user}
+  return {user: state.user};
 })
-(({ user}) => {
+(({user}) => {
   return <div>User:{user.name}</div>;
 });
 
--- src/redux.tsx
+++ src/redux.tsx
@@ -4,7 +4,8 @@ export const appContext = React.createContext<any>(null);
 
 export const store: any = {
   state: {
-    user: {name: 'sheben', age: 20}
+    user: {name: 'sheben', age: 20},
+    group: {name: '前端组'}
   },
   setState(newState: any) {
     store.state = newState;
@@ -34,16 +35,34 @@ const reducer = (state: any, {type, payload}: { type: string, payload: any }) =>
   }
 };
 
+const changed = (oldState: any, newState: any) => {
+  let changed = false;
+  for (let key in oldState) {
+    if (oldState[key] !== newState[key]) {
+      changed = true;
+    }
+  }
+  return changed;
+};
+
 export const connect = (selector?: any) => (Component: React.FC<any>) => {
   return (props: any) => {
     const {state, setState} = useContext(appContext);
     const [, update] = useState({});
     const data = selector ? selector(state) : {state: state};
     useEffect(() => {
-      store.subscribe(() => {
-        update({});
+      const unSubscribe = store.subscribe(() => {
+        const newData = selector ? selector(store.state) : {state: store.state};
+        if (changed(data, newData)) {
+          console.log('update');
+          update({});
+        }
       });
-    }, []);
+      // 这里最好加一个取消订阅，否则在 selector 变化时会出现重复订阅
+      // unSubscribe 这个函数只用到了一次，因此可以省略函数名，直接 return store.subscribe
+      // 不过那么写不易理解，因此不做那个改动了
+      return unSubscribe
+    }, [selector]);
     const dispatch = (action: any) => {
       setState(reducer(state, action));
     };
