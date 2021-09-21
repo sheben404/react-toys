9ac4c98b742cc30de7c3eeb12a0581ae2c085599
sheben404 <sheben404@gmail.com>
2021-09-21 12:21:50 +0800

    init: 使用 jest 完成添加、toggle、删除 todo 测试用例

---
+++ test/app.test.ts
@@ -0,0 +1,27 @@
+import {Todo} from '../src/reducer';
+import store from '../src/store';
+
+it('可以添加一条 todo', () => {
+  const newTodo: Todo = {
+    id: 99,
+    text: '吃饭',
+    state: 'todo'
+  }
+  store.dispatch({type: 'addTodo', payload: newTodo})
+  const todos = store.getState().todos
+  expect(todos[todos.length - 1]).toEqual(newTodo)
+})
+
+it('可以完成一条 todo', () => {
+  const toggleId = 99
+  store.dispatch({type: 'toggleTodo', payload: toggleId})
+  const todos = store.getState().todos
+  expect(todos[todos.length - 1].state).toEqual('done')
+})
+
+it('可以删除一条 todo', () => {
+  const delId = 99
+  store.dispatch({type: 'removeTodo', payload: delId})
+  const todos = store.getState().todos
+  expect(todos.length).toEqual(3)
+})
