1bb9ea70c990b64d75787fcf152b96e8f0525def
sheben404 <sheben404@gmail.com>
2021-09-21 16:07:02 +0800

    feat: 引入 redux-thunk 实现异步 action

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,15 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import ReduxThunk from 'redux-thunk';
 import todosReducer from './todos/reducer';
 import filterReducer from './filter/reducer';
+
 
 const reducer = combineReducers({
   todos: todosReducer,
   filter: filterReducer
-})
+});
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(ReduxThunk));
 
 store.subscribe(() => console.log('update component'));
 
